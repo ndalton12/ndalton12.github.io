b820e460ca7a7d6da0ee693b143f17ce088c2924
ndalton12 <niall.dalton12@gmail.com>
2026-01-12 23:14:28 +0000

    Updates

--- component---src-pages-404-jsx-02960415a027bcce3933.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkndalton_website=self.webpackChunkndalton_website||[]).push([[256],{7291:function(e,t,n){n.r(t);var l=n(7294),a=n(1883),s=n(1988),o=n(4839);const u="text-lg font-bold",c="underline";t.default=()=>l.createElement(s.Z,null,l.createElement(o.Z,{title:"Not found"}),l.createElement("h1",{className:u},"404: Not Found"),l.createElement("p",null,"You just hit a route that doesn't exist."," ",l.createElement(a.Link,{className:c,to:"/"},"Return to safety"),"."))}}]);
-//# sourceMappingURL=component---src-pages-404-jsx-02960415a027bcce3933.js.map
